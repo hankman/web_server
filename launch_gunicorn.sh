8f324013081f378e80bb8c4e2940a5ea2909450d
c-fan <c-fan@outlook.com>
2022-04-12 10:16:22 +0800

    move python env to 3.8.10

--- launch_gunicorn.sh
+++ launch_gunicorn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /home/cfan/pyenvs/pyenv1/bin/activate
+source /home/cfan/pyenvs/pyenv38/bin/activate
 
 pushd /home/cfan/web_server/ > /dev/null
 gunicorn -c gunicorn_conf.py main:app
