37eebd844abcdbd850a74964f6d6f59389bac508
Chen Fan <c-fan@outlook.com>
2022-04-18 22:28:21 +0800

    Introduce redis to speed up query

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
-import pickle
 import pandas as pd
-import datetime as dt
 import json
-from math import log
-from flask import Flask, render_template, Response
+from itertools import chain
+import io
+
+from flask import Flask, Response
 from jinja2 import FileSystemLoader, Environment
+import redis
 
 
 with open(os.path.expanduser('/etc/web_server/server.json')) as f:
@@ -19,6 +20,8 @@ DATA_FILE = os.path.join(DATA_DIR, 'infect.pickle')
 CNT_FILE = os.path.join(DATA_DIR, 'cnt.pickle')
 DEBUG_FILE = os.path.join(DATA_DIR, 'grab_infect_data.html')
 
+REDIS = redis.Redis(host='localhost', port=6379, db=0)
+
 
 template_loader = FileSystemLoader(searchpath='./templates/')
 template_env =Environment(loader=template_loader)
@@ -27,55 +30,16 @@ template_env =Environment(loader=template_loader)
 app = Flask(__name__)
 
 
-def vague_search_print(comm, data):
-    if comm != '*':
-        return data[data.Community.str.contains(comm)]
-    else:
-        return data
-
-
-def init_all_data():
-    with open(DATA_FILE, 'rb') as f:
-        all_data = pickle.load(f)
-    update_date = all_data.Date.max().strftime('%Y-%m-%d')
-    now = dt.datetime.combine(dt.date.today(), dt.time())
-    passed_days = (now - all_data.groupby(['Dist', 'Community']).Date.max()).dt.days
-    remaining_days = 14 - passed_days
-    remaining_days.loc[remaining_days <= 0] = 0
-    all_data = all_data.merge(
-        remaining_days.rename('Remain').reset_index(), on=['Dist', 'Community'])
-    all_data['Remain_str'] = all_data.Remain.astype(str) + ' 天'
-    all_data['Date_str'] = pd.to_datetime(all_data.Date).dt.strftime('%-m月%-d日')
-    return all_data, update_date, passed_days
+def get_updated_date():
+    return REDIS.get('updated_date').decode('utf8')
 
 
 def init_cnt_data():
-    with open(CNT_FILE, 'rb') as f:
-        cnt_data = pickle.load(f)
-    df = cnt_data.loc[cnt_data.index.unique().sort_values(
-        ascending=False)[:5]].fillna(0)
-    df['全市'] = df.sum(axis=1)
-    df = df.reset_index().set_index(['Date', 'Kind']).unstack(
-        ).T.fillna(0).astype(int)
+    return REDIS.get('cnt_summary').decode('utf8')
 
-    df.columns = [d.strftime('%-m月%-d日') for d in df.columns]
-    df.index.names = (None, None)
-    df_log = df.applymap(lambda x: log(x) if x != 0 else 0)
-    styled_df = df.sort_index().style.background_gradient(
-        axis=None, cmap='Oranges', text_color_threshold=0,
-        subset=([i for i in df.index if i[1] == 'BL' and i[0] != '全市'], df.columns),
-        gmap=df_log, high=0.85).background_gradient(
-            axis=None, cmap='Blues', text_color_threshold=0,
-            subset=([i for i in df.index if i[1] == 'WZZ' and i[0] != '全市'], df.columns),
-            gmap=df_log, high=0.85).background_gradient(
-                axis=None, cmap='Purples', text_color_threshold=0,
-                subset=([i for i in df.index if i[1] == 'BL' and i[0] == '全市'], df.columns),
-                high=0.85, low=0.25).background_gradient(
-                    axis=None, cmap='Greens', text_color_threshold=0,
-                    subset=([i for i in df.index if i[1] == 'WZZ' and i[0] == '全市'], df.columns),
-                    high=0.85, low=0.25).format_index(
-                        formatter=lambda x: '确诊' if x == 'BL' else '无症状', level=1)
-    return styled_df.to_html()
+
+def init_dist_data():
+    return REDIS.get('dist_summary').decode('utf8')
 
 
 def get_file_mime_type(file):
@@ -104,49 +68,34 @@ def init_file_data(directory):
     return file_data
 
 
-def get_result_data(place, all_data):
-    place = place.strip()
-    if place[-1] == '弄' or place[-1] == '号':
-        place = place[:-1]
-    data = vague_search_print(place, all_data)
-    return data
+def search_data(place):
+    places = [i.strip() for i in place.split('|')]
+    places = [place[:-1] if place[-1] == '弄' or place[-1] == '号' else place for place in places]
+    return b''.join(chain.from_iterable([REDIS.mget(REDIS.keys('*{}*'.format(j))) for j in places]))
 
 
-def get_result_html(place, all_data):
-    data = get_result_data(place, all_data)
-    if data.shape[0] == 0:
-        return '<span>(无感染记录或未收录地址。注意，输入地址请勿包含行政区或者楼栋。)</span>'
-    else:
-        data = data.sort_values(['Dist', 'Community', 'Date'], ascending=[True, True, False]).rename(
-            columns={'Dist': '区', 'Community': '地址', 'Date_str': '报告日期', 'Remain_str': '解封剩余'})
+def get_result_html(place):
+    data = search_data(place)
 
-        return data[['区', '地址', '解封剩余', '报告日期']].set_index(
-            ['区', '地址', '解封剩余', '报告日期']).style.to_html()
+    if len(data) == 0:
+        return ''
 
+    data = pd.read_csv(
+        io.BytesIO(data),
+        names=['Dist', 'Community', 'Date'])
+    data['Date_obj'] = pd.to_datetime(data['Date'])
+    data = data.sort_values(
+        ['Dist', 'Community', 'Date_obj'], ascending=[True, True, False]
+        )[['Dist', 'Community', 'Date']]
 
-def init_dist_data(all_data):
-    dist_summary = all_data[
-        all_data.Date.isin(sorted(all_data.Date.unique())[-5:])
-    ].groupby(['Dist', 'Date']).size().rename('Counts').sort_index(
-    ).reset_index().set_index(['Dist', 'Date']).unstack()
-    dist_summary = pd.concat([dist_summary, dist_summary.sum().rename('全市').to_frame().T])
-
-    dist_summary.columns = [d[1].strftime('%-m月%-d日') for d in dist_summary.columns]
-    dist_summary.index.name = None
-    dist_summary = dist_summary.sort_index().style.background_gradient(
-        axis=None, cmap='Oranges', high=0.85, text_color_threshold=0, subset=(
-            [i for i in dist_summary.index if i != '全市'], dist_summary.columns))
-    dist_summary = dist_summary.background_gradient(
-        axis=None, cmap='Blues', high=0.85, low=0.25, text_color_threshold=0,
-        subset=(['全市'], dist_summary.columns))
-    return dist_summary.to_html()
+    ret = data.to_csv(index=False, header=False)
+    return ret
 
 
-ALL_DATA, UPDATE_DATE, PASSED_DAYS = init_all_data()
+UPDATE_DATE = get_updated_date()
 CNT_DATA = init_cnt_data()
-DIST_DATA = init_dist_data(ALL_DATA)
+DIST_DATA = init_dist_data()
 RESOURCES_DATA = init_file_data(RESOURCES_DIR)
-
 
 
 @app.route('/old/')
@@ -204,6 +153,12 @@ def processing_backend():
             ) for line in f]))
 
 
+@app.route('/search_data/<place>')
+@app.route('/old/search_data/<place>')
+def query_data(place):
+    return get_result_html(place)
+
+
 @app.route('/debug')
 def debug_page():
     with open(DEBUG_FILE, 'rb') as f:
@@ -217,9 +172,8 @@ def backend():
 
 @app.route('/old/search/<place>')
 def old_search_page(place):
-    return old_search_page.PAGE_TEMPLATE.format(
-        title='"{}"的查询结果'.format(place),
-        table_content=get_result_html(place, ALL_DATA))
+    return old_search_page.PAGE_TEMPLATE
+
 
 old_search_page.PAGE_TEMPLATE = template_env.get_template('main.html').render(
     long_search=True, extra_notice="，将本页发送到桌面以快速查询",
@@ -228,32 +182,13 @@ old_search_page.PAGE_TEMPLATE = template_env.get_template('main.html').render(
 
 @app.route('/search/<place>')
 def search_page(place):
-    return search_page.PAGE_TEMPLATE.format(
-    title='"{}"的查询结果'.format(place),
-    table_content=get_result_html(place, ALL_DATA))
+    return search_page.PAGE_TEMPLATE
 
 search_page.PAGE_TEMPLATE = template_env.get_template('main.html').render(
     long_search=True, extra_notice="，将本页发送到桌面以快速查询",
     update_date=UPDATE_DATE,
     url_prefix='https://hankman.github.io/chenfan_info_web_resources',
     relative_to_root='..')
-
-
-@app.route('/old/iframe_search/<place>')
-def old_iframe_search(place):
-    return old_iframe_search.PAGE_TEMPLATE.format(
-        table_content=get_result_html(place, ALL_DATA))
-
-old_iframe_search.PAGE_TEMPLATE = template_env.get_template(
-    'search-iframe.html').render(url_prefix='')
-
-
-@app.route('/iframe_search/<place>')
-def iframe_search(place):
-    return iframe_search.PAGE_TEMPLATE.format(table_content=get_result_html(place, ALL_DATA))
-
-iframe_search.PAGE_TEMPLATE = template_env.get_template('search-iframe.html').render(
-    url_prefix='https://hankman.github.io/chenfan_info_web_resources')
 
 
 @app.route('/resources/<file>')
