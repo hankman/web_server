a162a5af182bbba869edc67ed1aa6bd98a3212ae
Chen Fan <c-fan@outlook.com>
2022-05-28 22:42:02 +0800

    Support searching for bj data

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ CNT_FILE = os.path.join(DATA_DIR, 'cnt.pickle')
 DEBUG_FILE = os.path.join(DATA_DIR, 'grab_infect_data.html')
 
 REDIS = redis.Redis(host='localhost', port=6379, db=0)
+BJ_REDIS = redis.Redis(host='localhost', port=6379, db=1)
 
 
 template_loader = FileSystemLoader(searchpath='./templates/')
@@ -30,8 +31,8 @@ template_env =Environment(loader=template_loader)
 app = Flask(__name__)
 
 
-def get_updated_date():
-    return REDIS.get('updated_date').decode('utf8')
+def get_updated_date(data_server):
+    return data_server.get('updated_date').decode('utf8')
 
 
 def init_cnt_data():
@@ -44,6 +45,10 @@ def init_dist_data():
 
 def init_gj_data():
     return REDIS.get('gj_summary').decode('utf8')
+
+
+def init_bj_dist_data():
+    return BJ_REDIS.get('dist_summary').decode('utf8')
 
 
 def get_file_mime_type(file):
@@ -72,34 +77,42 @@ def init_file_data(directory):
     return file_data
 
 
-def search_data(place):
+def search_data(place, data_server):
     places = [i.strip() for i in place.split('|')]
     places = [place[:-1] if place[-1] == '弄' or place[-1] == '号' else place for place in places]
-    return b''.join(chain.from_iterable([REDIS.mget(REDIS.keys('*{}*'.format(j))) for j in places]))
+    return b''.join(chain.from_iterable([data_server.mget(data_server.keys('*{}*'.format(j))) for j in places]))
 
 
-def get_result_html(place):
-    data = search_data(place)
+def get_result_html_sorted(place, data_server, col_list):
+    data = search_data(place, data_server)
 
     if len(data) == 0:
         return ''
 
-    data = pd.read_csv(
-        io.BytesIO(data),
-        names=['Dist', 'Community', 'Date'])
+    data = pd.read_csv(io.BytesIO(data), names=col_list)
     data['Date_obj'] = pd.to_datetime(data['Date'])
     data = data.sort_values(
         ['Dist', 'Community', 'Date_obj'], ascending=[True, True, False]
-        )[['Dist', 'Community', 'Date']]
+        )[col_list]
 
     ret = data.to_csv(index=False, header=False)
     return ret
+
+
+def get_result_html(place):
+    return get_result_html_sorted(place, REDIS, ['Dist', 'Community', 'Date'])
+
+
+def bj_get_result_html(place):
+    return get_result_html_sorted(place, BJ_REDIS, ['Dist', 'Community', 'Date', 'QZ', 'WZZ'])
+
 
 
 CNT_DATA = init_cnt_data()
 DIST_DATA = init_dist_data()
 GJ_DATA = init_gj_data()
 RESOURCES_DATA = init_file_data(RESOURCES_DIR)
+BJ_DIST_DATA = init_bj_dist_data()
 
 
 @app.route('/old/')
@@ -108,7 +121,16 @@ def old_root():
 
 old_root.PAGE = template_env.get_template('main.html').render(
     search=True, logo_sameline=True, url_prefix='', relative_to_root='.',
-    switch_notice='。 切换至<a href="/">新网页</a>')
+    switch_notice='。 切换至<a href="../">新网页</a>')
+
+
+@app.route('/bj/old/')
+def old_bj_root():
+    return old_bj_root.PAGE
+
+old_bj_root.PAGE = template_env.get_template('main.html').render(
+    search=True, logo_sameline=True, url_prefix='', relative_to_root='.',
+    switch_notice='。 切换至<a href="../">新网页</a>', bj=True)
 
 
 @app.route('/')
@@ -119,7 +141,19 @@ root.PAGE = template_env.get_template('main.html').render(
     search=True, logo_sameline=True,
     url_prefix='https://hankman.github.io/chenfan_info_web_resources',
     relative_to_root='.',
-    switch_notice='。 切换回<a href="/old/">旧版网页</a>')
+    switch_notice='。 切换回<a href="./old/">旧版网页</a>')
+
+
+@app.route('/bj/')
+def bj_root():
+    return bj_root.PAGE
+
+bj_root.PAGE = template_env.get_template('main.html').render(
+    search=True, logo_sameline=True,
+    url_prefix='https://hankman.github.io/chenfan_info_web_resources',
+    relative_to_root='.',
+    switch_notice='。 切换回<a href="./old/">旧版网页</a>',
+    bj=True)
 
 
 @app.route('/old/dist/')
@@ -145,6 +179,29 @@ dist_summary.PAGE = template_env.get_template('main.html').render(
     ).replace('{title}', '各行政区感染数据统计')
 
 
+@app.route('/bj/old/dist/')
+def bj_old_dist_summary():
+    return bj_old_dist_summary.PAGE
+
+bj_old_dist_summary.PAGE = template_env.get_template('main.html').render(
+        dist=True, dist_table=BJ_DIST_DATA,
+        url_prefix='', relative_to_root='..', bj=True
+    ).replace('{title}', '各行政区感染数据统计')
+
+
+@app.route('/bj/dist/')
+def bj_dist_summary():
+    return bj_dist_summary.PAGE
+
+bj_dist_summary.PAGE = template_env.get_template('main.html').render(
+        dist=True,
+        dist_table=BJ_DIST_DATA,
+        url_prefix='https://hankman.github.io/chenfan_info_web_resources',
+        relative_to_root='..',
+        bj=True
+    ).replace('{title}', '各行政区感染数据统计')
+
+
 def processing_backend():
     with open(__file__, 'r') as f:
         return '<html><body>{}</body></html>'.format(
@@ -161,6 +218,12 @@ def processing_backend():
 @app.route('/old/search_data/<place>')
 def query_data(place):
     return get_result_html(place)
+
+
+@app.route('/bj/search_data/<place>')
+@app.route('/bj/old/search_data/<place>')
+def bj_query_data(place):
+    return bj_get_result_html(place)
 
 
 @app.route('/data_processing')
@@ -184,6 +247,16 @@ old_search_page.PAGE_TEMPLATE = template_env.get_template('main.html').render(
     url_prefix='', relative_to_root='..')
 
 
+@app.route('/bj/old/search/<place>')
+def bj_old_search_page(place):
+    return bj_old_search_page.PAGE_TEMPLATE
+
+
+bj_old_search_page.PAGE_TEMPLATE = template_env.get_template('main.html').render(
+    long_search=True, extra_notice="，将本页发送到桌面以快速查询",
+    url_prefix='', relative_to_root='..', bj=True)
+
+
 @app.route('/search/<place>')
 def search_page(place):
     return search_page.PAGE_TEMPLATE
@@ -192,6 +265,16 @@ search_page.PAGE_TEMPLATE = template_env.get_template('main.html').render(
     long_search=True, extra_notice="，将本页发送到桌面以快速查询",
     url_prefix='https://hankman.github.io/chenfan_info_web_resources',
     relative_to_root='..')
+
+
+@app.route('/bj/search/<place>')
+def bj_search_page(place):
+    return search_page.PAGE_TEMPLATE
+
+bj_search_page.PAGE_TEMPLATE = template_env.get_template('main.html').render(
+    long_search=True, extra_notice="，将本页发送到桌面以快速查询",
+    url_prefix='https://hankman.github.io/chenfan_info_web_resources',
+    relative_to_root='..', bj=True)
 
 
 @app.route('/resources/<file>')
@@ -218,12 +301,22 @@ def legacy_query1(place):
     return '服务器已升级，请重新加载页面'
 
 
-@app.route('/update_date/')
-@app.route('/old/update_date/')
-def update_date():
-    response = Response(get_updated_date())
+def get_update_date_resp(data_server):
+    response = Response(get_updated_date(data_server))
     response.cache_control.no_cache = True;
     return response
+
+
+@app.route('/update_date/')
+@app.route('/old/update_date/')
+def sh_update_date():
+    return get_update_date_resp(REDIS)
+
+
+@app.route('/bj/update_date/')
+@app.route('/bj/old/update_date/')
+def bj_update_date():
+    return get_update_date_resp(BJ_REDIS)
 
 
 @app.after_request
